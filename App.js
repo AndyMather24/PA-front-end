13939b58942d33081b27fe08fe15c6344227d1f2
Andrew Mather <Andy-mather@live.co.uk>
2018-11-26 15:29:20 +0000

    event structure

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.header}>
+        </View>
+        <View style={styles.main} />
+        <View style={styles.footer} />
       </View>
     );
   }
@@ -13,9 +16,21 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  header: {
+    flex: 1,
+    backgroundColor: 'powderblue'
+  },
+
+  main: {
+    flex: 6,
+    backgroundColor: 'skyblue',
+  },
+
+  footer: {
+    flex: 1,
+    backgroundColor: 'steelblue',
+
+  }
 });
