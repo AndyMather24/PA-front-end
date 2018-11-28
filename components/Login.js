438a6bfc70b1e7bb4129fa1dc40a253cf4aa9c04
nathanscott85 <nathansfeely@gmail.com>
2018-11-28 09:23:30 +0000

    remove branch

---
+++ components/Login.js
@@ -0,0 +1,10 @@
+import {React, Component } from "react"
+import {ScrollView, Text, View, Button} from "react-native";
+
+export default class Login extends Component {
+    render(){
+        return (
+            <Text>This is a login Page</Text>
+        )
+    }
+}
