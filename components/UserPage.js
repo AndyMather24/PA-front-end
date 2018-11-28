ad79e57d3d63c0af19c8153aef2d63460ec3d9c0
rigorsaif <31529436+rigorsaif@users.noreply.github.com>
2018-11-28 13:00:26 +0000

    add user component

---
+++ components/UserPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+class User extends Component {
+  static navigationOptions = {
+    title: "Me!"
+  };
+  state = {
+    username: "peterPlant",
+    avatar:
+      "https://pickaface.net/gallery/avatar/unr_goateebald_170216_1834_yvoxe.png"
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.imgContainer}>
+          <Image
+            style={styles.img}
+            source={{
+              uri:
+                "https://pickaface.net/gallery/avatar/unr_goateebald_170216_1834_yvoxe.png"
+            }}
+          />
+        </View>
+        <View style={styles.settings}>
+          <Text>{this.state.username}</Text>
+          <Button title="Sign out" onPress={() => this.props.navigation.navigate("Auth")}/>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  img: {
+    height: 100,
+    width: 100,
+    borderRadius: 20
+  },
+  imgContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  settings: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+export default User;
