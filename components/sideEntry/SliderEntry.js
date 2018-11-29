0191dc4d43509c3a78973d76183c77b1479eab3d
Andrew Mather <Andy-mather@live.co.uk>
2018-11-29 17:06:16 +0000

    add toggle class based on state.active

--- components/sideEntry/SliderEntry.js
+++ components/sideEntry/SliderEntry.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { ParallaxImage } from 'react-native-snap-carousel';
-import styles from '../styles/SliderEntry'; 
- class SliderEntry extends Component {
-    
+import styles from '../styles/SliderEntry';
+// import styles from "../styles/ToggledSlider";
+class SliderEntry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
     static propTypes = {
         data: PropTypes.object.isRequired,
         even: PropTypes.bool,
@@ -12,56 +18,61 @@ import styles from '../styles/SliderEntry';
         parallaxProps: PropTypes.object
     };
 
-    get image () {
+    get image() {
         const { data: { illustration }, parallax, parallaxProps, even } = this.props;
 
         return parallax ? (
             <ParallaxImage
-              source={{ uri: illustration }}
-              containerStyle={[styles.imageContainer, even ? styles.imageContainerEven : {}]}
-              style={[styles.image, { position: 'relative' }]}
-              parallaxFactor={0.35}
-              showSpinner={true}
-              spinnerColor={even ? 'rgba(255, 255, 255, 0.4)' : 'rgba(0, 0, 0, 0.25)'}
-              {...parallaxProps}
+                source={{ uri: illustration }}
+                containerStyle={[styles.imageContainer, even ? styles.imageContainerEven : {}, this.state.active && styles.toggleImageContainer]}
+                style={[styles.image, { position: 'relative' }]}
+                parallaxFactor={0.35}
+                showSpinner={true}
+                spinnerColor={even ? 'rgba(255, 255, 255, 0.4)' : 'rgba(0, 0, 0, 0.25)'}
+                {...parallaxProps}
             />
         ) : (
-            <Image
-              source={{ uri: illustration }}
-              style={styles.image}
-            />
-        );
+                <Image
+                    source={{ uri: illustration }}
+                    style={styles.image}
+                />
+            );
     }
 
-    render () {
+    render() {
         const { data: { title, subtitle }, even } = this.props;
 
         const uppercaseTitle = title ? (
             <Text
-              style={[styles.title, even ? styles.titleEven : {}]}
-              numberOfLines={2}
+                style={[styles.title, even ? styles.titleEven : {}]}
+                numberOfLines={2}
             >
-                { title.toUpperCase() }
+                {title.toUpperCase()}
             </Text>
         ) : false;
 
         return (
             <TouchableOpacity
-              activeOpacity={1}
-              style={styles.slideInnerContainer}
-              onPress={() => { alert(`You've clicked '${title}'`); }}
-              >
-                <View style={[styles.imageContainer, even ? styles.imageContainerEven : {}]}>
-                    { this.image }
+                activeOpacity={1}
+                style={[styles.slideInnerContainer, this.state.active && styles.toggleSlideInnerContainer]}
+                onPress={() => {
+                    console.log("clicked")
+                    this.setState({
+                        active: !this.state.active
+                    })
+                }}
+            >
+                <View style={[styles.imageContainer, even ? styles.imageContainerEven : {}, this.state.active && styles.toggleImageContainer]}>
+                    {this.image}
                     <View style={[styles.radiusMask, even ? styles.radiusMaskEven : {}]} />
                 </View>
-                <View style={[styles.textContainer, even ? styles.textContainerEven : {}]}>
-                    { uppercaseTitle }
+                <View style={[styles.textContainer, even ? styles.textContainerEven : {}, this.state.active && styles.textContainer]}>
+                    {uppercaseTitle}
                     <Text
-                      style={[styles.subtitle, even ? styles.subtitleEven : {}]}
-                      numberOfLines={2}
+                        style={[styles.subtitle, even ? styles.subtitleEven : {}]}
+                        numberOfLines={2}
                     >
-                        { subtitle }
+                        {subtitle}
                     </Text>
                 </View>
             </TouchableOpacity>
