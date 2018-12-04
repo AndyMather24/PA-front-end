3424f8681de7dec71bba78beb52fc5e190423e82
nathanscott85 <nathansfeely@gmail.com>
2018-12-04 10:29:20 +0000

    update location images

--- components/static/locationImages.js
+++ components/static/locationImages.js
@@ -1,9 +1,20 @@
 const locationImages = {
-    Manchester: "https://www.visitmanchester.com/imageresizer/?image=%2fdmsimgs%2fchristmas-markets-at-albert-square_1__441639496.jpg&action=ProductDetailFullWidth2",
-    York: 'https://i.dailymail.co.uk/i/pix/2017/06/27/15/41CEF4B500000578-4624966-image-m-4_1498575389876.jpg',
-    London: 'https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/07/10/11/Future-London.jpg?w968',
-    Newcastle: 'http://l12couriers.co.uk/wp-content/uploads/2016/01/Newcastle-Couriers.jpg',
-    Bristol: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bristol_Marina.jpg/1200px-Bristol_Marina.jpg'
-}
+  Manchester:
+    "https://www.visitmanchester.com/imageresizer/?image=%2fdmsimgs%2fchristmas-markets-at-albert-square_1__441639496.jpg&action=ProductDetailFullWidth2",
+  York:
+    "https://i.dailymail.co.uk/i/pix/2017/06/27/15/41CEF4B500000578-4624966-image-m-4_1498575389876.jpg",
+  London:
+    "https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/07/10/11/Future-London.jpg?w968",
+  Newcastle:
+    "http://l12couriers.co.uk/wp-content/uploads/2016/01/Newcastle-Couriers.jpg",
+  Bristol:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bristol_Marina.jpg/1200px-Bristol_Marina.jpg",
+  Leeds:
+    "https://aspenwoolf.co.uk/wp-content/uploads/2017/07/Why-Leeds-is-The-Shining-Star-of-Investment-Opportunity.jpg",
+  Edinburgh: "https://speedybooker.com/pool/edinburgh-university-rooms.jpg",
+  Liverpool:
+    "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/liverpool-city-and-river-mersey-steve-kearns.jpg",
+  Birmingham: "https://www.birmingham.ac.uk/Images/News/dubai-image.jpg"
+};
 
 export default locationImages;
